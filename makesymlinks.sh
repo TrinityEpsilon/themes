f7b4cd29bf2263037c6fc4bdc030d6efd5914a07
TrinityEpsilon <jeffersonwchen@yahoo.com>
2016-07-22 20:08:58 -0700

    Renamed

---
+++ makesymlinks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# copy.sh
+# links all files to their respective locations
+# ideally run in root mode
+
+# defining variables:
+
+echo "Please give the location of the themes directory: "
+read dir                                                                                          # themes directory
+themes="Arc Arc-Dark Arc-Darker Default FlatStudioDark Vertex Vertex-Dark"                        # themes list
+icons="Breeze-Amber bridge vertex-icons-master Vertex-Icons"                                      # icons list
+
+# copies all themes into /usr/share/themes
+echo "Moving themes into the /usr/share/themes directory..."
+for theme in $themes; do
+	ln -s -f $dir/$theme /usr/share/themes/$theme
+done
+# copies all icons into /usr/share/icons
+for icon in $icons; do
+	ln -s -f $dir/$icon /usr/share/icons/$icon
+done
+
+# configures lightdm
+ln -s -f $dir/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+
+# copies pictures into their locations
+cp -f $dir/Pictures/wallpapers/Ibex.png /usr/share/lightdm/backgrounds/Ibex.png
+ln -s -f $dir/Pictures ~/Pictures
